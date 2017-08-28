06f212d1943f52a44aca4db0b12497ac742f4a59
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 10:55:18 +0100

    Import code

---
+++ app.cmake
@@ -0,0 +1,9 @@
+message(STATUS "Loading tests-crypto module...")
+
+if (NOT CONFIGURED_ONCE)
+	set(APP_TESTS_CRYPTO_SOURCE_DIR ${APP_TESTS_CRYPTO_DIR}/src)
+endif()
+
+add_executable(aurorafw_app_tests_crypto ${APP_TESTS_CRYPTO_SOURCE_DIR}/test_crypto.cpp)
+
+set_target_properties(aurorafw_app_tests_crypto PROPERTIES OUTPUT_NAME test_crypto.out)
