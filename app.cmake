689c6c26ac01d924ef14f907fa90407f014b26b8
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 16:43:30 +0100

    Fix app and library link

--- app.cmake
+++ app.cmake
@@ -6,4 +6,6 @@ endif()
 
 add_executable(aurorafw_app_tests_crypto ${APP_TESTS_CRYPTO_SOURCE_DIR}/test_crypto.cpp)
 
+target_link_libraries(aurorafw_app_tests_crypto aurorafw-core aurorafw-cli aurorafw-crypto)
+
 set_target_properties(aurorafw_app_tests_crypto PROPERTIES OUTPUT_NAME test_crypto.out)
